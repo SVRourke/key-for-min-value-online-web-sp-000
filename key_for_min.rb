6c4323d9342c1d16f79ac3f0b669461ca9e213b4
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 17:34:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  buff_key = ""
+  buff_val = ""
+  
+  name_hash.each do |key, value|
+    if buff_val
+      if key < buff_val
+        buff_key = key
+        buff_val = value
+      end
+    end
+  end
+  
 
 end
